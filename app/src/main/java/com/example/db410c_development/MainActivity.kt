d3245df522b1b01cb5d6fc509b58dc9787726feb
deepakreddy <deepak.jonnalagadda@gmail.com>
2020-03-31 11:18:58 +0530

    Implemented getGpioPortList() as a class.

--- app/src/main/java/com/example/db410c_development/MainActivity.kt
+++ app/src/main/java/com/example/db410c_development/MainActivity.kt
@@ -11,29 +11,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getPortList()
+
+        val gpioPortList = gpioController()
+        gpioPortList.getPortList()
     }
 
-
-    fun getPortList(){
-        Log.i(TAG, "Getting Available GPIO Port List")
-        val directory = File(PATH)
-        val files = directory.list {directory, name -> name.startsWith("gpio")}
-        for ((index, item) in files.withIndex())
-        {
-            if (index >12){
-                gpioArrayList.add(item.toUpperCase())
-            }
-        }
-        Log.i(TAG, "Available GPIO Ports are: ${gpioArrayList[0]}, ${gpioArrayList[1]}, ${gpioArrayList[2]}, ${gpioArrayList[3]}, " +
-                "${gpioArrayList[4]}, ${gpioArrayList[5]}, ${gpioArrayList[6]}, " +
-                "${gpioArrayList[7]}, ${gpioArrayList[8]}, ${gpioArrayList[9]}, ${gpioArrayList[10]}")
-    }
 
     companion object{
         val TAG = "funGpioController"
         val PATH = "/sys/class/gpio/"
         var gpioArrayList = ArrayList<String>()
+    }
+
+    class gpioController {
+
+        fun getPortList(){
+            Log.i(TAG, "Getting GPIO Port List")
+            val directory = File(PATH)
+            val files = directory.list {directory, name -> name.startsWith("gpio")}
+            for ((index, item) in files.withIndex())
+            {
+                if (index >12){
+                    gpioArrayList.add(item.toUpperCase())
+                }
+            }
+            Log.i(TAG, "GPIO Ports are: ${gpioArrayList[0]}, ${gpioArrayList[1]}, ${gpioArrayList[2]}, ${gpioArrayList[3]}, " +
+                    "${gpioArrayList[4]}, ${gpioArrayList[5]}, ${gpioArrayList[6]}, " +
+                    "${gpioArrayList[7]}, ${gpioArrayList[8]}, ${gpioArrayList[9]}, ${gpioArrayList[10]}")
+        }
     }
 }
 
